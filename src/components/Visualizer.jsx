18106fffeaa59428c7f65c68c718800010e95ae3
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-09 01:26:00 -0400

    setup empty nodes and grid

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,5 +1,42 @@
-const Visualizer = () => {
-  return <div>hello world</div>;
-};
+import React, { Component } from "react";
+import Node from "./node/Node";
+import "./visualizer.css";
 
-export default Visualizer;
+export default class Visualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+    };
+  }
+
+  componentDidMount() {
+    const nodes = [];
+    for (let row = 0; row < 15; row++) {
+      const currentRow = [];
+      for (let column = 0; column < 50; column++) {
+        currentRow.push([]);
+      }
+      nodes.push(currentRow);
+    }
+    this.setState({ nodes });
+  }
+
+  render() {
+    const { nodes } = this.state;
+
+    return (
+      <div className="grid">
+        {nodes.map((row, rowIdx) => {
+          return (
+            <div>
+              {row.map((node, nodeIdx) => (
+                <Node></Node>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
---
+++ src/components/node/Node.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./node.css";
+
+export default class Node extends Component {
+  render() {
+    return <div className="node"></div>;
+  }
+}

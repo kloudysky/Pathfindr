c7d5811b4c0acacc7eb810bd8ca11e1ce325925b
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-09 02:22:34 -0400

    configuring nodes for grid

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -2,32 +2,55 @@ import React, { Component } from "react";
 import Node from "./node/Node";
 import "./visualizer.css";
 
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
+
 export default class Visualizer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes: [],
+      grid: [],
     };
   }
 
   componentDidMount() {
-    const nodes = [];
-    for (let row = 0; row < 15; row++) {
-      const currentRow = [];
-      for (let column = 0; column < 50; column++) {
-        currentRow.push([]);
-      }
-      nodes.push(currentRow);
-    }
-    this.setState({ nodes });
+    const grid = this.createInitialGrid();
+    this.setState({ grid });
   }
 
+  createInitialGrid = () => {
+    const grid = [];
+    for (let row = 0; row < 15; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push(this.createNode(row, col));
+      }
+      grid.push(currentRow);
+    }
+    return grid;
+  };
+
+  createNode = (row, col) => {
+    return {
+      row,
+      col,
+      isStart: row === START_NODE_ROW && col === START_NODE_COL,
+      isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+    };
+  };
+
   render() {
-    const { nodes } = this.state;
+    const { grid } = this.state;
 
     return (
       <div className="grid">
-        {nodes.map((row, rowIdx) => {
+        {grid.map((row, rowIdx) => {
           return (
             <div>
               {row.map((node, nodeIdx) => (
--- src/components/node/Node.jsx
+++ src/components/node/Node.jsx
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 import "./node.css";
 
 export default class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
     return <div className="node"></div>;
   }
 }
